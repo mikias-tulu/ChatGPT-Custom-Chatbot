ced55d7870e0df0675bf661596691e342fc2adfd
mikias-tulu <miki.tulu@gmail.com>
2023-07-06 02:24:15 +0300

    chatCompletion function to test the api

---
+++ index.js
@@ -0,0 +1,22 @@
+import { Configuration, OpenAIApi } from 'openai';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+const openai = new OpenAIApi(configuration);
+
+async function main() {
+    const chatCompletion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            { role:'user', content: 'what is Ethiopia best known for?' }
+        ]
+    });
+
+    console.log(chatCompletion.data.choices[0]);
+}
+
+main ();
