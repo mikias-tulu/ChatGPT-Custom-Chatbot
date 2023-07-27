56c7cd489160f76fee56db17fbf390577f508ccf
mikias-tulu <miki.tulu@gmail.com>
2023-07-27 21:00:17 +0300

    Call the API with user input

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-import openai from './config/open-ai.js';
-import readlineSync from 'readline-sync';
-import colors from 'colors';
-
+import openai from "./config/open-ai.js";
+import readlineSync from "readline-sync";
+import colors from "colors";
 
 async function main() {
-    console.log(colors.bold.green('Hello there!, Welcome to Terminal Custom Chatbot!'));
-    console.log(colors.bold.green('You can start chatting with the bot.'));
+  console.log(
+    colors.bold.green("Hello there!, Welcome to Terminal Custom Chatbot!")
+  );
+  console.log(colors.bold.green("You can start chatting with the bot."));
 
-    const chatCompletion = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo',
-        messages: [
-            { role:'user', content: 'what is Ethiopia best known for?' }
-        ]
-    });
+  
+  while (true) {
+    const userInput = readlineSync.question(colors.yellow("You: "));
 
-    console.log(chatCompletion.data.choices[0]);
+    try {
+      
+      // Add latest user input
+      messages.push({ role: "user", content: userInput });
+
+      // Call the API with user input
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+      });
+
+      } catch (error) {
+      console.error(colors.red(error));
+    }
+  }
 }
 
-main ();
+main();
