7a47e879e3907069af29601bf468c6bb963eb591
mikias-tulu <miki.tulu@gmail.com>
2023-07-27 20:56:18 +0300

    Removed duplicated code

--- index.js
+++ index.js
@@ -2,17 +2,11 @@ import openai from './config/open-ai.js';
 import readlineSync from 'readline-sync';
 import colors from 'colors';
 
-import { Configuration, OpenAIApi } from 'openai';
-import dotenv from 'dotenv';
-dotenv.config();
-
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY
-});
-
-const openai = new OpenAIApi(configuration);
 
 async function main() {
+    console.log(colors.bold.green('Hello there!, Welcome to Terminal Custom Chatbot!'));
+    console.log(colors.bold.green('You can start chatting with the bot.'));
+
     const chatCompletion = await openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
         messages: [
